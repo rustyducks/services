766cf91478ecb03d44efc00a34c6f8b3d0847d0e
Crolonome <crolonome@rustyducks.fr>
2022-05-12 11:09:49 +0200

    Improve installer.

--- install_services.sh
+++ install_services.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# install udev rules
+sudo cp 49-rustyducks.rules /etc/udev/rules.d/49-rustyducks.rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+# install services
 sudo ln -s /home/pi/services/duckbase.service /etc/systemd/system/duckbase.service
 sudo ln -s /home/pi/services/duckio.service /etc/systemd/system/duckio.service
 sudo systemctl enable duckbase.service
 sudo systemctl enable duckio.service
-sudo cp 49-rustyducks.rules /etc/udev/rules.d/49-rustyducks.rules
+
