0e6ac5a39c424fb45c5041f20c27c009788ab9d8
Crolonome <crolonome@rustyducks.fr>
2022-05-24 22:52:12 +0200

    Adds lidar and crolnome as services

--- install_services.sh
+++ install_services.sh
@@ -8,6 +8,9 @@ sudo udevadm trigger
 # install services
 sudo ln -s /home/pi/services/duckbase.service /etc/systemd/system/duckbase.service
 sudo ln -s /home/pi/services/duckio.service /etc/systemd/system/duckio.service
+sudo ln -s /home/pi/services/lidar.service /etc/systemd/system/lidar.service
+sudo ln -s /home/pi/services/coincrolonome.service /etc/systemd/system/coincrolonome.service
 sudo systemctl enable duckbase.service
 sudo systemctl enable duckio.service
-
+sudo systemctl enable lidar.service
+sudo systemctl enable coincrolonome.service
