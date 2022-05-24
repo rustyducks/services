469ed918fdea510cd18d801e43514f8064787414
Dalek <dalek@rustyduck.fr>
2022-05-24 22:02:43 +0100

    Adds coindalek in service installation

--- install_services.sh
+++ install_services.sh
@@ -9,8 +9,15 @@ sudo udevadm trigger
 sudo ln -s /home/pi/services/duckbase.service /etc/systemd/system/duckbase.service
 sudo ln -s /home/pi/services/duckio.service /etc/systemd/system/duckio.service
 sudo ln -s /home/pi/services/lidar.service /etc/systemd/system/lidar.service
-sudo ln -s /home/pi/services/coincrolonome.service /etc/systemd/system/coincrolonome.service
 sudo systemctl enable duckbase.service
 sudo systemctl enable duckio.service
 sudo systemctl enable lidar.service
-sudo systemctl enable coincrolonome.service
+if [ $1 == "dalek" ]; then
+	sudo ln -s /home/pi/services/coindalek.service /etc/systemd/system/coindalek.service
+	sudo systemctl enable coindalek.service
+elif [ $1 == "crolonome" ]; then
+	sudo ln -s /home/pi/services/coincrolonome.service /etc/systemd/system/coincrolonome.service
+	sudo systemctl enable coincrolonome.service
+else
+	echo "Usage ./install_services.sh [dalek|crolonome]"
+fi
